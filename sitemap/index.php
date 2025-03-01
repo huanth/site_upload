9ef92b7148fb64df25ea62a6f68a7393416bfad6
HuanTH <huan.huy.truong@gmail.com>
2025-03-02 01:35:16 +0700

    Update code

--- sitemap/index.php
+++ sitemap/index.php
@@ -2,30 +2,35 @@
 // Config database
 include '../config/config.php';
 
-$base_url = "https://" . $home_url . "/sitemap"; // Đường dẫn gốc của sitemap
-$sitemap_file = __DIR__ . "/sitemap.xml"; // Đường dẫn lưu file sitemap.xml
+try {
+    $base_url = "https://" . $home_url . "/sitemap"; // Đường dẫn gốc của sitemap
+    $sitemap_file = __DIR__ . "/sitemap.xml"; // Đường dẫn lưu file sitemap.xml
 
-// Lấy thời gian hiện tại theo UTC+7
-$date = new DateTime("now", new DateTimeZone("Asia/Bangkok"));
-$lastmod = $date->format("Y-m-d");
+    // Lấy thời gian hiện tại theo UTC+7
+    $date = new DateTime("now", new DateTimeZone("Asia/Bangkok"));
+    $lastmod = $date->format("Y-m-d");
 
-// Tạo XML pages
-$sitemapIndex = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></sitemapindex>');
+    // Tạo XML pages
+    $sitemapIndex = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></sitemapindex>');
 
-// Danh sách các sitemap con
-$sitemaps = [
-    "sitemap-pages.xml",
-];
+    // Danh sách các sitemap con
+    $sitemaps = [
+        "sitemap-pages.xml",
+    ];
 
-// Thêm từng sitemap con vào sitemap index
-foreach ($sitemaps as $sitemap) {
-    $sitemapItem = $sitemapIndex->addChild('sitemap');
-    $sitemapItem->addChild('loc', "$base_url/$sitemap");
-    $sitemapItem->addChild('lastmod', $lastmod);
+    // Thêm từng sitemap con vào sitemap index
+    foreach ($sitemaps as $sitemap) {
+        $sitemapItem = $sitemapIndex->addChild('sitemap');
+        $sitemapItem->addChild('loc', "$base_url/$sitemap");
+        $sitemapItem->addChild('lastmod', $lastmod);
+    }
+
+    // Ghi XML ra file
+    $sitemapIndex->asXML($sitemap_file);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
 
-// Ghi XML ra file
-$sitemapIndex->asXML($sitemap_file);
 
 // XML Pages
 include './generate_sitemap_pages.php';
