dcffefb9fe2d50b72c80c28339bc9a568b293835
HuanTH <huan.huy.truong@gmail.com>
2025-03-01 11:05:57 +0700

    Update thông báo

--- tin-tuc.php
+++ tin-tuc.php
@@ -6,22 +6,19 @@
             <div class="bg-red-500 text-white text-xs font-bold px-2 py-1 absolute right-0 top-0">Ghim</div>
             <div class="p-4">
                 <a href="news/7" class="news-title block text-xl font-bold text-success hover:text-accent mb-3 transition-colors border-l-4 border-primary pl-3 py-1 bg-gray-50">
-                    TEAMOBI BẢO TRÌ CỔNG NẠP THẺ CÀO </a>
-                <p class="text-gray-600 text-sm mb-3">◦•●◉✿ THÔNG BÁO ✿◉●•◦
-                    💟 &lt; BẢO TRÌ CỔNG NẠP THẺ CÀO&gt; 💟
-                    Chào các bạn,
-                    Cổng nạp THẺ CÀO hiện đang...</p>
+                    Thông báo từ Admin </a>
+                <p class="text-gray-600 text-sm mb-3">Thông báo mở website upload</p>
                 <div class="news-meta text-xs text-gray-500 mb-3 flex justify-between">
                     <span class="flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                         </svg>
-                        puaruvn </span>
+                        huanth </span>
                     <span class="flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                         </svg>
-                        11/02/2025 07:42 </span>
+                        01/03/2025 07:42 </span>
                 </div>
             </div>
         </div>
