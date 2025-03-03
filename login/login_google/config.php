1c1bfdb035e38f94780488fe89edc370033ab4b6
Huan Truong [ONEXT] <huanth@onextdigital.com>
2025-03-03 14:13:29 +0700

    Update code

--- login/login_google/config.php
+++ login/login_google/config.php
@@ -3,4 +3,4 @@
 
 define('GOOGLE_CLIENT_ID', '1057751685086-qlpdknb9995tfu7gs0u874s1hnafm4lv.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URI', 'http://tuyetvoi.xyz/login/login_google/callback.php');
+define('GOOGLE_REDIRECT_URI', 'https://tuyetvoi.xyz/login/login_google/callback.php');
