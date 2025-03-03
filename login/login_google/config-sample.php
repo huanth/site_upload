500f02c4c1257f53a056eaa23c74ee74de5c48fe
Huan Truong [ONEXT] <huanth@onextdigital.com>
2025-03-03 15:37:57 +0700

    .gitignore is now working

---
+++ login/login_google/config-sample.php
@@ -0,0 +1,6 @@
+<?php
+// login_google/config.php
+
+define('GOOGLE_CLIENT_ID', 'YOUR_GOOGLE_CLIENT_ID');
+define('GOOGLE_CLIENT_SECRET', 'YOUR_GOOGLE_CLIENT_SECRET');
+define('GOOGLE_REDIRECT_URI', 'url_callback'); // Đảm bảo URI này chính xác
--- login/login_google/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// login_google/config.php
-
-define('GOOGLE_CLIENT_ID', '1057751685086-qlpdknb9995tfu7gs0u874s1hnafm4lv.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URI', 'https://tuyetvoi.xyz/login/login_google/callback.php');
