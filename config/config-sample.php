fe6e04c5da0c57948bb91f899433e6195d93ef7c
Huan Truong [ONEXT] <huanth@onextdigital.com>
2025-03-03 08:31:55 +0700

    Upload new config sample

---
+++ config/config-sample.php
@@ -0,0 +1,20 @@
+<?php
+// Config database
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'upload';
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
+
+
+// Load config
+$sql_config = "SELECT * FROM config WHERE `key` = 'home_url'";
+$result_config = mysqli_query($conn, $sql_config);
+$config = mysqli_fetch_assoc($result_config);
+
+// Config
+$home_url = $config['value'];
