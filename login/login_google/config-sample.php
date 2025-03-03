21d483fc32a53c34428be7ebf063d0cfc505b4e2
Huan Truong [ONEXT] <huanth@onextdigital.com>
2025-03-03 15:44:02 +0700

    Update code\

--- login/login_google/config-sample.php
+++ login/login_google/config-sample.php
@@ -3,4 +3,4 @@
 
 define('GOOGLE_CLIENT_ID', 'YOUR_GOOGLE_CLIENT_ID');
 define('GOOGLE_CLIENT_SECRET', 'YOUR_GOOGLE_CLIENT_SECRET');
-define('GOOGLE_REDIRECT_URI', 'url_callback'); // Đảm bảo URI này chính xác
+define('GOOGLE_REDIRECT_URI', 'url_callback'); // Đảm bảo URI này chính xác 
